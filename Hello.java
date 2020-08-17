e013669117b2276b29e1a238d04cc91c9f655670
nguyenvt35 <69822014+nguyenvt35@users.noreply.github.com>
2020-08-17 17:20:46 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/****************************************
+Hello.java
+*****************************************
+*Vivian Nguyen
+*8/17/20
+*CMSC 255-C90
+*****************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Vivian Nguyen");
+		System.out.println("Hello.java");
+		System.out.println("This is the code for Lab 01. This introductory program is designed to introduce me!");
+		System.out.println("8/17/20");
+		System.out.println("CMSC 255-C90");
+		System.out.println();
+	}
+}
